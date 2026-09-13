d77c71593d4686c7fddbf2106463d0cc25fbd10b
Kevin Quick <kquick@galois.com>
2026-09-13 14:42:04 -0700

    Regenerate static cabal2nix derivations on flake update.

---
+++ nix/named-text.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, base, bytestring, deepseq, hashable, hspec
+, lib, parameterized-utils, prettyprinter, sayable, tasty
+, tasty-ant-xml, tasty-checklist, tasty-hspec, template-haskell
+, text, unordered-containers
+}:
+mkDerivation {
+  pname = "named-text";
+  version = "1.2.5.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base deepseq hashable prettyprinter sayable template-haskell
+    text
+  ];
+  testHaskellDepends = [
+    aeson base bytestring hspec parameterized-utils prettyprinter
+    sayable tasty tasty-ant-xml tasty-checklist tasty-hspec text
+    unordered-containers
+  ];
+  description = "A parameterized named text type and associated functionality";
+  license = lib.licenses.isc;
+}
